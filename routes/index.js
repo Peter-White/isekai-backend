962a58a2fa3821fdbf3f731ea9d6875f622b1f58
Peter-White <pwhitedeveloper@gmail.com>
2021-11-18 15:11:27 -0600

    user controller and route begin

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const { Router } = require('express');
 const router = Router();
+const userRoutes = require('./user');
 
 router.get('/', (req, res) => res.send('This is root!'));
+router.get("/users", userRoutes);
 
 module.exports = router;
---
+++ routes/user.js
@@ -0,0 +1,7 @@
+const { Router } = require('express');
+const router = Router();
+
+router.get("/", (req, res) => res.send("Users"));
+router.get("/:id", (req, res) => res.send("User"));
+
+module.exports = router;
